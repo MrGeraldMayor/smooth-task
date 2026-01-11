684341301a92de3a76eda438ca45ddea19251178
MrGeraldMayor <hisshass988@gmail.com>
2026-01-11 20:19:43 +0100

    Add files via upload

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fl7bj3u2np8"
+    "revision": "0.l9icb083loo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
